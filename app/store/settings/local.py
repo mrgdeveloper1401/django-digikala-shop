d501118f221deef9b4b7b0b64b32bb264106488f
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-11-18 09:57:48 +0330

    connect database postgresql

--- app/store/settings/local.py
+++ app/store/settings/local.py
@@ -4,20 +4,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': os.environ.get('DATABASE-NAME', 'django_shop'),
-#         'USER': os.environ.get('DATABASE-USER', 'django_shop'),
-#         'PASSWORD': os.environ.get('DATABASE-PASSWORD', 'django_shop.123456'),
-#         'HOST': os.environ.get('DATABASE-HOST', 'database'),
-#         'PORT': os.environ.get('DATABASE-PORT', 5432)
-#     }}
-
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": "db.sqlite3",
-    }
-}
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE-NAME', 'django_shop'),
+        'USER': os.environ.get('DATABASE-USER', 'django_shop'),
+        'PASSWORD': os.environ.get('DATABASE-PASSWORD', 'django_shop.1234'),
+        'HOST': os.environ.get('DATABASE-HOST', '127.0.0.1'),
+        'PORT': os.environ.get('DATABASE-PORT', 5432)
+    }}
+
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": "db.sqlite3",
+#     }
+# }
