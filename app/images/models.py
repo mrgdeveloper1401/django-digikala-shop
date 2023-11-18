e09cad87c7e0492c095579ade27acd691ea2e44d
mohammad goodarzi <m.goodarzi606@gmail.com>
2023-11-18 08:27:30 +0330

    install pip

--- app/images/models.py
+++ app/images/models.py
@@ -6,6 +6,10 @@ from common.models import CreateModel, UpdateModel
 class ImagesModel(CreateModel, UpdateModel):
     image = models.ImageField(_('عکس'), upload_to='images/%Y/%m/%d/')
     display_order = models.PositiveIntegerField(default=0)
+    alter_image = models.CharField(_('توضیح در مورد عکس'), max_length=50)
+    width_image = models.SmallIntegerField(('عرض'))
+    height_image = models.SmallIntegerField(('ارتفاع'))
+    
     
     class Meta:
         verbose_name = _('image')
